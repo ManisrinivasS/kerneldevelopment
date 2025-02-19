57fdf58ab2d7dbbc2233c32f2f40a25b5b133d04
Manisrinivas <manisrinivas.ece@gmail.com>
2025-02-19 21:23:14 +0530

    Added description to the API's

--- final_char_driver.c
+++ final_char_driver.c
@@ -68,6 +68,9 @@ static int __init fcd_init(void)
 	int ret;
 	int i ;
 	struct device *dev_ret;
+// alloc_chrdev_region - to register and dynamically allocate devices range of numbers - 
+// Devices will be identified by number not with symbolic name
+// MAJOR and MINOR - will assign dynamically, minor number will start usally from the zero value.
 
 	if ((ret = alloc_chrdev_region(&dev, FIRST_MINOR, MINOR_CNT, "final_driver")) < 0)
 	{
@@ -83,11 +86,11 @@ static int __init fcd_init(void)
 		unregister_chrdev_region(dev, MINOR_CNT);
 		return PTR_ERR(cl);
 	}
-	
+        // loop for two minor devices	
 	for(i=0;i<2;i++){
-	cdev_init(&c_dev[i], &driver_fops);
+	cdev_init(&c_dev[i], &driver_fops);//Initalize the character device , and initialize file operations structure driver_fops
 	c_dev[i].owner = THIS_MODULE;
-
+        //cdev_add registers a device with the kernel, MKDEV- will holds the major and minor numbers and helps us to create the dev_t identifies for the           device
 	if ((ret = cdev_add(&c_dev[i], MKDEV(MAJOR(dev), i), 1)) < 0)
 	{
 		unregister_chrdev_region(dev, MINOR_CNT);
@@ -95,10 +98,9 @@ static int __init fcd_init(void)
 	}
 
 	
-	//TODO 2: Create the device & assign it to dev_ret
+	//TODO 2: Create the device & assign it to dev_ret                            
 	//Refer include/linux/device.h for device_create() prototype. 2nd & 4th arguments are NULL
-  dev_ret=device_create(cl,NULL,MKDEV(MAJOR(dev), i),NULL,"finalchar%d",i);
-
+  dev_ret=device_create(cl,NULL,MKDEV(MAJOR(dev), i),NULL,"finalchar%d",i);// create dev file in /dev/mychar*, now user applicatins can  interact with kernel driver via a device file operations
 //   struct device *device_create(, struct device *parent,vdev_t devt, 
 // 								void *drvdata, const char *fmt, ...);
 
