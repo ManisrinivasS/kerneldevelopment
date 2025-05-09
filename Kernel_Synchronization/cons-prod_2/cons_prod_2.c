e7b4ccfd417dcd1aafc90695f5fab9bc8482cb3b
Manisrinivas <manisrinivas.ece@gmail.com>
2025-05-09 20:06:31 +0530

    Bounder buffer cons prod

--- Kernel_Synchronization/cons-prod_2/cons_prod_2.c
+++ Kernel_Synchronization/cons-prod_2/cons_prod_2.c
@@ -17,173 +17,143 @@
 #include <linux/semaphore.h>
 
 
+#define BUF_SIZE 5
+int buffer[BUF_SIZE];
+static int head=0, tail=0;
 
-static struct task_struct *thread_cons[2], *thread_prod;
+static struct task_struct *thread_cons[2], *thread_prod[2];
 static int thread_id[2];
+static int thread_id_prod[2];
 
-struct job 
-{
-	int Mydata;
-	struct job* next;
-};
+struct semaphore my_semEmpty;
+struct semaphore my_semFull;
+struct mutex my_mutex;
 
-static struct job* job_queue;
-DEFINE_MUTEX(my_mutex);
-DEFINE_SEMAPHORE(my_sem);
 
-static void process_job(void *thread_id, struct job* next_job)
-{
-	unsigned int i, id;
-	if (thread_id == NULL) 
-	{
-		printk("Invalid Thread id\n");
-		return;
-	}
-	id = *(int *)thread_id;
-	printk("\nThe data from the thread %u is %d\n",id, next_job->Mydata);
-	get_random_bytes(&i, sizeof(i));
-	ssleep(i % 5);
+static void enqueue_job(int val){
+  buffer[tail]=val;
+  tail = (tail+1)%BUF_SIZE;
+
 }
 
-void enqueue_job (struct job* new_job)
-{
-	static int i = 0;
-	mutex_lock(&my_mutex);
-	new_job->next = job_queue;
-	job_queue = new_job;
-	job_queue->Mydata=i++;
-	mutex_unlock(&my_mutex);
-	up(&my_sem);//signal data is available
-		printk("\n Job no %d is added.\n",i-1);
+static int dequeue(void){
+    int val= buffer[head];
+    head=(head+1) % BUF_SIZE;
+    return val;
 }
 
-int producer(void* arg)
-{
-	struct job *new_job;
-	unsigned int i;
+static int producerthread(void *arg){
+    unsigned int i;
+    int id=*(int *)arg;
+    unsigned int j;
+    unsigned int rand_val;
 
-	allow_signal(SIGKILL);
+    while(!kthread_should_stop()){
 
-	while (!kthread_should_stop())
-	{
-		new_job=(struct job*)kmalloc(sizeof(struct job), GFP_KERNEL);
-		if (new_job == NULL) 
-		{
-			printk("Unable to allocate the job structure\n");
-			return -1;
-		}
+        get_random_bytes(&rand_val, sizeof(rand_val));
+        rand_val %=  100; 
+       
+        down_interruptible(&my_semEmpty); 
+        mutex_lock(&my_mutex);
+        enqueue_job(rand_val);
+        printk(KERN_INFO "Producer %d: produced item %d\n", id, rand_val);
+        mutex_unlock(&my_mutex);
+        up(&my_semFull);
 
-		enqueue_job(new_job);
-		get_random_bytes(&i, sizeof(i));
-		ssleep(i % 5);
-		if (signal_pending(current))
+        get_random_bytes(&j, sizeof(j));
+		ssleep(j % 3);
+       if (signal_pending(current))
 			break;
-	}
-	printk(KERN_INFO "Producer Thread Stopping\n");
-	thread_prod = NULL;
+   }
+    
+    printk(KERN_INFO "Producer Thread Stopping\n");
+    thread_prod[id] = NULL;
 	do_exit(0);
 }
 
-static int thread_fn(void *thread_id)
-{
-	int id;
-    int res=0;
-	allow_signal(SIGKILL);
-
-	while (!kthread_should_stop()) 
-	{
-		struct job* next_job;
-
-		res=down_interruptible(&my_sem); 
+static int consumerthread(void *arg){
+     int res;
+     int id;
+     unsigned int i;
+     
+     while(!kthread_should_stop()){
+        down_interruptible(&my_semFull); 
         mutex_lock(&my_mutex);
-
-		if (job_queue == NULL)
-			next_job = NULL;
-		else
-		{
-			next_job = job_queue;
-			job_queue = job_queue->next;
-		}
-
-		mutex_unlock(&my_mutex);
-		
-
-		if (next_job == NULL)
-			continue;
-		process_job (thread_id, next_job);
-		kfree (next_job);
-		if (signal_pending(current))
+        res=dequeue();
+        printk("counsmer data:%d\n",res);
+        mutex_unlock(&my_mutex);
+        up(&my_semEmpty);
+       
+        get_random_bytes(&i, sizeof(i));
+		ssleep(i % 2);
+        if (signal_pending(current))
 			break;
-	}
-	id = *(int *)thread_id;
-	thread_cons[id] = NULL;
+     }
+
+     id = *(int *)arg;
+ 	thread_cons[id] = NULL;
 	printk(KERN_INFO "Consumer Thread Stopping\n");
 	do_exit(0);
 }
 
 
-static int __init init_consprod(void)
-{
-	int i;
-	char buff[20];
-	struct job *new_job;
+static int __init init_consprod(void){
+    printk("module init\n");
 
-	for (i = 0; i < 6; i++) 
-	{
-		new_job=(struct job*)kmalloc(sizeof(struct job), GFP_KERNEL);
-		if (new_job == NULL) {
-			printk("Failed to allocate the memory for job\n");
-			return -1;
-		}
-		enqueue_job(new_job);
-	}
 
-	for (i = 0; i < 2; i++) 
-	{
-		thread_id[i] = i;
-		sprintf(buff, "thread_con%d", i);
-		thread_cons[i] = kthread_run(thread_fn, &thread_id[i], buff);
-		if (thread_cons[i])
-			printk(KERN_INFO "Thread %d created\n", i);
-		else 
-		{
-			printk(KERN_INFO "Thread creation failed\n");
-			return -1;
-		}
-	}
-	thread_prod = kthread_run(producer, NULL, "myprod");
-	if (thread_prod)
-		printk(KERN_INFO "Producer Thread created\n");
-	else 
-	{
-		printk(KERN_INFO "Producer thread creation failed\n");
-		return -1;
-	}
+    sema_init(&my_semEmpty, BUF_SIZE);
+    sema_init(&my_semFull,0);
+    mutex_init(&my_mutex);
+    int i,j;
 
-	return 0;
+//-------thread creation for consumer-----------------//
+    for( i = 0 ; i <2;i++){
+        thread_id[i]=i;
+        thread_cons[i]=kthread_run(consumerthread,&thread_id[i],"my_consumer");
+        if(thread_cons[i]){
+            printk(KERN_INFO "Consumer thread %d created\n",i);
+        }else{
+            printk(KERN_INFO "Consumer threadd failed\n");
+            return -1;
+        }
+    }
+//-------thread creation for producer-----------------//
+
+    for( j =0;j<2;j++){
+        thread_id_prod[j] = j;
+        thread_prod[j] = kthread_run(producerthread,&thread_id_prod[j],"my_producer");
+        if(thread_prod[j]){
+            printk(KERN_INFO "Producer thread %d created", j);
+        }else{
+            printk(KERN_INFO "Failed to create producer thread\n");
+            return -1;
+        }
+    }
+
+    return 0;
 }
 
-static void __exit cleanup_consprod(void)
-{
-	int i;
-	printk("Cleaning Up\n");
-	for (i = 0; i < 2; i++)
-	{
-		if (thread_cons[i] != NULL)
-		{
-			kthread_stop(thread_cons[i]);
-			printk(KERN_INFO "Thread %d stopped", i);
-		}
-	}
-	if (thread_prod != NULL)
-	{
-		kthread_stop(thread_prod);
-		printk(KERN_INFO "Thread prod stopped");
-	}
+static void __exit cleanup_consprod(void){
+    printk("module cleaning up\n");
+    int i;
+    up(&my_semFull);   // Wake sleeping consumers
+    up(&my_semEmpty); 
+    for( i=0;i<2;i++){
+
+        if(thread_cons[i] != NULL){
+            kthread_stop(thread_cons[i]);
+            printk(KERN_INFO "consumer thread %d stopped",i);
+        }
+        if(thread_prod[i] != NULL){
+            kthread_stop(thread_prod[i]);
+            printk(KERN_INFO "producer thread %d stopped",i);
+        }
+       
+    }
 }
 
 module_init(init_consprod);
 module_exit(cleanup_consprod);
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Kernel drivers learnings");
-MODULE_DESCRIPTION("Consumer Producer Demo");
+MODULE_AUTHOR("Kernel drivers learning");
+MODULE_DESCRIPTION("Bounder buffer consumer producer problem");
